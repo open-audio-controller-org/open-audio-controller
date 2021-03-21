2fa0de78cb8139a02ea94e6492c5a5cd73cc3699
RuikisR <raivoruikis@gmail.com>
2021-03-21 15:21:43 +0000

    Fixed: Python package caching

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM debian:latest
 RUN apt update
 RUN apt-get install -y python3-pip
 RUN pip3 install --upgrade pip
+COPY requirements.txt requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt
 COPY . /open-audio-controller
-WORKDIR /open-audio-controller
-RUN pip3 install -r requirements.txt
-WORKDIR tests
+WORKDIR /open-audio-controller/tests
 RUN coverage run test_suite.py
 ENTRYPOINT ["coverage", "report"]
--- tests/test_example_name.py
+++ tests/test_example_name.py
@@ -1,20 +1,20 @@
 import unittest
 '''
 Mandatory import for unit testing capability
-The code you want to test should also be imported here
+The code you want to test should also be imported here.
 '''
 
 
 class TestGroupName(unittest.TestCase):
     '''
     Use a class which extends unittest.TestCase to
-    group your tests together
+    group your tests together.
     '''
 
     def setUp(self):
         '''
         setUp() is used to declare variables or initialise data
-        structures to be used across the test group
+        structures to be used across the test group.
         '''
         self.val_a = 1
         self.val_b = 1
@@ -23,7 +23,7 @@ class TestGroupName(unittest.TestCase):
         '''
         The other functions declared within the class are your tests
         which will be comprised of self.assert* type calls, passing
-        the appropriate parameters and a meaningful failure message
+        the appropriate parameters and a meaningful failure message.
         '''
         self.assertEqual(self.val_a, self.val_b,
                          "This would be displayed if the test failed")
@@ -33,7 +33,7 @@ class TestGroupName(unittest.TestCase):
         The opposite of setUp(), tearDown() can be used to deallocate
         resources which generally won't be necessary unless testing
         things with connection streams which require explicitly calling
-        some form of .close() method
+        some form of .close() method.
         '''
         pass
 
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     The tests in this file will automatically be discovered and
     run in a suite by the main test file, but these tests can be run on their
     own by including this ifmain, which calls unittest.main() and simply
-    running this file
+    running this file.
     '''
     unittest.main()
 """
