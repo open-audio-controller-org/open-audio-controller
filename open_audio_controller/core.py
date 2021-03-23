ab13f75a1f569a9189237a4c14b7086479a41663
Sarah Klein <pklein@tcd.ie>
2021-03-23 10:31:28 -0400

    Implementation: core.py
    Providing and templating basic I/O functionality.
    
    Note: retains misc. testing code for demonstration and use.

---
+++ open_audio_controller/core.py
@@ -0,0 +1,67 @@
+"""core.py provides audio I/O and passes to audio processing"""
+
+import time
+import pyaudio, wave, numpy
+
+"""These to be replaced with user's settings"""
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+RECORD_TO_FILE = False
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "file.wav"
+
+audio_engine = pyaudio.PyAudio()
+audio_frames = []
+audio_stream = None
+
+"""Setup PyAudio"""
+def prepare():
+    audio_engine = pyaudio.PyAudio()
+
+"""Live Audio Input Record"""
+def start_stream():
+    audio_stream = audio_engine.open(format=FORMAT,
+                                     channels=CHANNELS,
+                                     rate=RATE,
+                                     output=True,
+                                     input=True,
+                                     stream_callback=stream_callback)
+    audio_stream.start_stream()
+
+"""Functionality for active stream"""
+def stream_callback(in_data, frame_count, time_info, flag):
+    audio_data = numpy.fromstring(in_data, dtype=numpy.float32)
+    if (RECORD_TO_FILE):
+        audio_frames.append(audio_data);
+    return audio_data, pyaudio.paContinue
+
+"""Live Audio Input Stop"""
+def end_stream():
+    audio_stream.stop_stream()
+
+"""File Audio Input Functionality"""
+def read_wave():
+    pass
+
+"""File Audio Output Functionality"""
+def save_wave():
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio_engine.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(audio_frames))
+    waveFile.close()
+
+"""Cleans up the audio engine"""
+def cleanup_engine():
+    if (audio_stream != None): audio_stream.close()
+    if (audio_engine != None): audio_engine.terminate()
+
+"""Main for Testing"""
+prepare()
+start_stream()
+time.sleep(10)
+end_stream()
+cleanup_engine()
