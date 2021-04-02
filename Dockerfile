227e3dd81063eec0040bb1955a521492156ea2fc
peadar-kenny <kennyp9@tcd.ie>
2021-04-02 11:42:12 +0100

    Add:POST request, EDIT:DockerFile to run app

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,13 @@ FROM debian:latest
 RUN apt update
 RUN apt-get install -y python3-pip portaudio19-dev
 RUN pip3 install --upgrade pip
-COPY requirements.txt requirements.txt
+COPY requirements.txt /app/requirements.txt
+WORKDIR /app
 RUN pip3 install --no-cache-dir -r requirements.txt
-COPY . /open-audio-controller
-WORKDIR /open-audio-controller/tests
-RUN coverage run test_suite.py
-ENTRYPOINT ["coverage", "report"]
+COPY . /app
+# WORKDIR /open-audio-controller/tests
+# RUN coverage run test_suite.py
+# ENTRYPOINT ["coverage", "report"]
+EXPOSE 5000
+ENTRYPOINT ["python3"]
+CMD ["open_audio_controller/__open-audio-flask__.py"]
--- open_audio_controller/__open-audio-flask__.py
+++ open_audio_controller/__open-audio-flask__.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, logging
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route('/controller_state', methods=['POST'])
+def controller_state():
+    content = request.json
+    state = content.state
+    if state == 'true':
+        app.logger.info('Turning on Module')
+    elif state == 'false':
+        app.logger.info('Turning off module')
+    else:
+        app.logger.info('No state found')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
